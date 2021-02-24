a170f0d31b9cf544f51f37943caecbd07fc83469
explodoboy <fiffason@gmail.com>
2021-02-24 00:55:27 -0800

    Initial Upload (v1.0)
    v1.0

---
+++ Source/SNSAOTC_LethalThresholdFix/SNSAOTC_LethalThresholdFix/LethalThresholdFix.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RimWorld;
+using HarmonyLib;
+using Verse;
+using System.Reflection;
+
+namespace SNSAOTC_LethalThresholdFix
+{
+    [StaticConstructorOnStartup]
+    static class Patch_LethalThreshold
+    {
+        static Patch_LethalThreshold()
+        {
+            new Harmony("explodoboy.SNSAmbitionCosmic").PatchAll();
+        }
+        [HarmonyPatch(typeof(Pawn_HealthTracker), nameof(Pawn_HealthTracker.LethalDamageThreshold), MethodType.Getter)]
+        class PatchHarmony_LethalThreshold
+        {
+            [HarmonyPostfix]
+            public static void LethalDamageThresholdPostfix(ref float __result)
+            {
+                __result = 24000f;
+            }
+        }
+    }
+}
